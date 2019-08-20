e2e3d05fb7cd2ac06e9b36814f6d35f07e088485
snolan98k <snolan98k@gmail.com>
2019-08-20 19:56:50 -0400

    Updates

---
+++ precache-manifest.a24efae651bc801b722cac4b61a134a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df29dc76d7c51b846042b209542b903",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3cfdfef05252e989c7a1",
+    "url": "/clicky-game/static/css/main.ee2caa58.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3cfdfef05252e989c7a1",
+    "url": "/clicky-game/static/js/main.3bb98643.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
